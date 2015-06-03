b08818c6dd4eceeb2a1f25404e32e53223d6b68b
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-06-03 11:36:29 -0700

    A bit more sketch

--- src/main/java/com/jenkins/plugins/rally/PostBuild.java
+++ src/main/java/com/jenkins/plugins/rally/PostBuild.java
@@ -2,6 +2,8 @@ package com.jenkins.plugins.rally;
 
 import hudson.model.Build;
 
+import java.net.URISyntaxException;
+
 /**
  * Shim to assist data migration.
  * 
@@ -21,7 +23,20 @@ public class PostBuild {
    	private String debugOn;
    	private String proxy;
 
+    /**
+     * Called when this is read from persisted form.
+     * Create equivalent {@link RallyPlugin} instance.
+     */
     public RallyPlugin readResolve() {
-        throw new UnsupportedOperationException();
+        try {
+            return new RallyPlugin(
+                "Pleasse set valid API key for "+userName,
+                workspace, scmRepoName,
+                null, null, null, null,null,null // TODO
+            );
+        } catch (Exception e) {
+            // not sure what the right thing to do here.
+            return null;
+        }
     }
 }
