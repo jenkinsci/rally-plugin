2d351791219f2636d7361ae34b1acfc5f611d499
R. Michael Rogers <r.m.rogers@gmail.com>
2015-08-12 08:52:20 -0400

    add tests for rallyservice
    there were no tests covering the task update logic -- this commit adds those tests.

--- src/test/java/com/jenkins/plugins/rally/service/RallyServiceTest.java
+++ src/test/java/com/jenkins/plugins/rally/service/RallyServiceTest.java
@@ -1,5 +1,6 @@
 package com.jenkins.plugins.rally.service;
 
+import com.google.gson.JsonObject;
 import com.jenkins.plugins.rally.RallyAssetNotFoundException;
 import com.jenkins.plugins.rally.RallyException;
 import com.jenkins.plugins.rally.config.AdvancedConfiguration;
@@ -7,14 +8,19 @@ import com.jenkins.plugins.rally.config.RallyConfiguration;
 import com.jenkins.plugins.rally.connector.RallyConnector;
 import com.jenkins.plugins.rally.connector.RallyUpdateData;
 import com.jenkins.plugins.rally.scm.ScmConnector;
+import com.jenkins.plugins.rally.utils.RallyQueryBuilder;
+import com.jenkins.plugins.rally.utils.RallyUpdateBean;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -70,5 +76,131 @@ public class RallyServiceTest {
         this.service.updateChangeset(details);
 
         verify(this.connector, never()).createRepository();
+    }
+
+    @Test
+    public void shouldDoNothingIfNoTasksDefined() throws RallyException {
+        RallyUpdateData details = new RallyUpdateData();
+
+        this.service.updateRallyTaskDetails(details);
+
+        verify(this.connector, never()).updateTask(anyString(), any(RallyUpdateBean.class));
+    }
+
+    @Test
+    public void shouldSetTaskStateToInProgressIfNoStateIsGiven() throws RallyException {
+        ArgumentCaptor<RallyUpdateBean> beanCaptor = ArgumentCaptor.forClass(RallyUpdateBean.class);
+        RallyUpdateData details = new RallyUpdateData();
+        details.addId("US12345");
+        details.setTaskID("TA54321");
+
+        JsonObject taskJsonObject = new JsonObject();
+        taskJsonObject.addProperty("_ref", "_ref");
+        when(this.connector.queryForStory(anyString())).thenReturn("_ref");
+        when(this.connector.queryForTaskById(anyString(), anyString())).thenReturn(new RallyQueryBuilder.RallyQueryResponseObject(taskJsonObject));
+
+        this.service.updateRallyTaskDetails(details);
+
+        verify(this.connector).updateTask(anyString(), beanCaptor.capture());
+
+        assertThat(beanCaptor.getValue().getJsonObject().get("State").getAsString(), containsString("In-Progress"));
+    }
+
+    @Test
+    public void shouldSetTaskStateToGivenState() throws RallyException {
+        ArgumentCaptor<RallyUpdateBean> beanCaptor = ArgumentCaptor.forClass(RallyUpdateBean.class);
+        RallyUpdateData details = new RallyUpdateData();
+        details.addId("US12345");
+        details.setTaskID("TA54321");
+        details.setTaskStatus("My Status");
+
+        JsonObject taskJsonObject = new JsonObject();
+        taskJsonObject.addProperty("_ref", "_ref");
+        when(this.connector.queryForStory(anyString())).thenReturn("_ref");
+        when(this.connector.queryForTaskById(anyString(), anyString())).thenReturn(new RallyQueryBuilder.RallyQueryResponseObject(taskJsonObject));
+
+        this.service.updateRallyTaskDetails(details);
+
+        verify(this.connector).updateTask(anyString(), beanCaptor.capture());
+
+        assertThat(beanCaptor.getValue().getJsonObject().get("State").getAsString(), containsString("My Status"));
+    }
+
+    @Test
+    public void shouldUpdateTaskTodoValue() throws RallyException {
+        ArgumentCaptor<RallyUpdateBean> beanCaptor = ArgumentCaptor.forClass(RallyUpdateBean.class);
+        RallyUpdateData details = new RallyUpdateData();
+        details.addId("US12345");
+        details.setTaskID("TA54321");
+        details.setTaskToDO("5");
+
+        JsonObject taskJsonObject = new JsonObject();
+        taskJsonObject.addProperty("_ref", "_ref");
+        when(this.connector.queryForStory(anyString())).thenReturn("_ref");
+        when(this.connector.queryForTaskById(anyString(), anyString())).thenReturn(new RallyQueryBuilder.RallyQueryResponseObject(taskJsonObject));
+
+        this.service.updateRallyTaskDetails(details);
+
+        verify(this.connector).updateTask(anyString(), beanCaptor.capture());
+
+        assertThat(beanCaptor.getValue().getJsonObject().get("ToDo").getAsString(), containsString("5"));
+    }
+
+    @Test
+    public void shouldCumulitivelyUpdateTaskActualHours() throws RallyException {
+        ArgumentCaptor<RallyUpdateBean> beanCaptor = ArgumentCaptor.forClass(RallyUpdateBean.class);
+        RallyUpdateData details = new RallyUpdateData();
+        details.addId("US12345");
+        details.setTaskID("TA54321");
+        details.setTaskActuals("5");
+
+        JsonObject taskJsonObject = new JsonObject();
+        taskJsonObject.addProperty("_ref", "_ref");
+        taskJsonObject.addProperty("Actuals", "3");
+        when(this.connector.queryForStory(anyString())).thenReturn("_ref");
+
+        when(this.connector.queryForTaskById(anyString(), anyString())).thenReturn(new RallyQueryBuilder.RallyQueryResponseObject(taskJsonObject));
+
+        this.service.updateRallyTaskDetails(details);
+
+        verify(this.connector).updateTask(anyString(), beanCaptor.capture());
+
+        assertThat(beanCaptor.getValue().getJsonObject().get("Actuals").getAsString(), containsString("8"));
+    }
+
+    @Test
+    public void shoudlUpdateTaskEstimate() throws RallyException {
+        ArgumentCaptor<RallyUpdateBean> beanCaptor = ArgumentCaptor.forClass(RallyUpdateBean.class);
+        RallyUpdateData details = new RallyUpdateData();
+        details.addId("US12345");
+        details.setTaskID("TA54321");
+        details.setTaskEstimates("3");
+
+        JsonObject taskJsonObject = new JsonObject();
+        taskJsonObject.addProperty("_ref", "_ref");
+        when(this.connector.queryForStory(anyString())).thenReturn("_ref");
+        when(this.connector.queryForTaskById(anyString(), anyString())).thenReturn(new RallyQueryBuilder.RallyQueryResponseObject(taskJsonObject));
+
+        this.service.updateRallyTaskDetails(details);
+
+        verify(this.connector).updateTask(anyString(), beanCaptor.capture());
+
+        assertThat(beanCaptor.getValue().getJsonObject().get("Estimate").getAsString(), containsString("3"));
+    }
+
+    @Test
+    public void shouldFetchTaskByIndex() throws RallyException {
+        RallyUpdateData details = new RallyUpdateData();
+        details.addId("US12345");
+        details.setTaskIndex("1");
+
+        JsonObject taskJsonObject = new JsonObject();
+        taskJsonObject.addProperty("_ref", "_ref");
+        when(this.connector.queryForStory(anyString())).thenReturn("_ref");
+        when(this.connector.queryForTaskByIndex(anyString(), eq(1))).thenReturn(new RallyQueryBuilder.RallyQueryResponseObject(taskJsonObject));
+
+        this.service.updateRallyTaskDetails(details);
+
+        verify(this.connector).updateTask(anyString(), any(RallyUpdateBean.class));
     }
 }
